144eacc6b295b4ab0b9c574ec8f9b45b37407b27
alvaro9rqc <aquispecondo@unsa.edu.pe>
2025-01-23 18:47:25 -0500

    Dinamic arrays

---
+++ linear-data-structure/DinamicArray.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main () {
+  vector<int> v; //sin elementos iniciales
+  v.push_back(1);
+  v.push_back(2);
+  v.push_back(3);
+  printf("%d\n", v[2]);
+  return 0;
+}
+
+
+
---
+++ linear-data-structure/DinamicArray.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class DinamicArray {
+  public static void main (String args[]) {
+    ArrayList<Integer> v = new ArrayList<Integer>();
+    v.add(1);
+    v.add(2);
+    v.add(3);
+    System.out.println(v.get(2));
+  }
+}
+
