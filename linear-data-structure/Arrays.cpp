19e8441e4b003300aaf9b0368e08cab2e92556d8
alvaro9rqc <aquispecondo@unsa.edu.pe>
2025-01-23 18:44:05 -0500

    Arrays státicos

---
+++ linear-data-structure/Arrays.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main () {
+  int arr[] = { 1, 2, 3 };
+  int arr2[3];
+  arr2[0] = 1;
+  printf("%d\n", arr2[0]);
+  printf("%d\n", arr[2]);
+
+  return 0;
+}
+
+
---
+++ linear-data-structure/Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays {
+
+  public static void main (String args[]) {
+    int arr[] = { 1, 2, 3 };
+    int arr2[] = new int[3];
+    arr2[0] = 1;
+    System.out.println(arr[2]);
+    System.out.println(arr2[2]);
+  }
+}
+
